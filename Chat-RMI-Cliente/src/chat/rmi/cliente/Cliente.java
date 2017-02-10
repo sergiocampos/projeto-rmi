80a9d603bfbc3cb57e090e280f2954ef1f74c2ae
Júnior Palitot <antonio.junior@tecw.com.br>
2017-02-10 15:36:12 -0300

    Nome em cliente

--- Chat-RMI-Cliente/src/chat/rmi/cliente/Cliente.java
+++ Chat-RMI-Cliente/src/chat/rmi/cliente/Cliente.java
@@ -10,10 +10,19 @@ package chat.rmi.cliente;
  * @author jrpalitot
  */
 public class Cliente implements ICliente{
+    public String nome;
+    
+    public Cliente (String nome){
+        this.nome = nome;
+    }
 
     @Override
     public void msg(String m) {
         System.out.println(m);
+    }
+    
+    public String getNome(){
+        return nome;
     }
     
 }
