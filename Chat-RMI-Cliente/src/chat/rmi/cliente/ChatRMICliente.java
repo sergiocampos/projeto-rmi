6b5d121b19777507273617a3244480553ede4010
Júnior Palitot <antonio.junior@tecw.com.br>
2017-02-14 17:41:48 -0300

    Cliente conecta servidor

--- Chat-RMI-Cliente/src/chat/rmi/cliente/ChatRMICliente.java
+++ Chat-RMI-Cliente/src/chat/rmi/cliente/ChatRMICliente.java
@@ -5,6 +5,14 @@
  */
 package chat.rmi.cliente;
 
+import chat.rmi.servidor.IServidor;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author jrpalitot
@@ -15,8 +23,16 @@ public class ChatRMICliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String host_url = "rmi://192.168.10.120";
-        
+        try {
+            String host_url = "rmi://192.168.10.120";
+
+            Cliente c = new Cliente ("Jr");
+            IServidor server;
+            server = (IServidor) Naming.lookup(host_url);
+            server.conecta(c);
+        } catch (RemoteException | NotBoundException | MalformedURLException ex) {
+            Logger.getLogger(ChatRMICliente.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
